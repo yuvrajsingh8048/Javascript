17a52024f94472317927906b5f7b445abed970d2
Yuvraj Singh <yuvrajsingh001750@gmail.com>
2023-11-03 13:45:35 -0700

    dateProject

---
+++ 01_Basics/7. dateProject.js
@@ -0,0 +1,36 @@
+const myDate = new Date()
+
+let date = myDate.getDate()
+if(date < 10){
+    date = "0" + date
+}
+
+let month = myDate.getMonth()
+if(month < 10){
+    month = "0" + month
+}
+let year = myDate.getFullYear()
+
+let weekday = ["Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let day = weekday[myDate.getDay()]
+
+let hour = myDate.getHours()
+
+let minutes = myDate.getMinutes()
+if(minutes < 10){
+    minutes = "0" + minutes
+}
+
+let seconds = myDate.getSeconds()
+if(seconds < 10){
+    seconds = "0" + seconds
+}
+
+let meridian = "AM"
+if(hour > 12){
+    hour =  hour - 12
+    hour = "0" + hour
+    meridian = "PM"
+}
+
+console.log(`${date}/${month}/${year} ${hour}:${minutes}:${seconds} ${meridian} ${day}`);
